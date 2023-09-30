59cf42ec530daa7238527faa0a719487ca7bd02c
Kyeremeh Prince <kyeremehprince68@gmail.com>
2023-09-30 12:54:41 +0000

    Merge NavList component with Navbar component

--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -1,11 +1,36 @@
-import NavList from "./NavList";
+import logoBlack from "./../assets/images/logo-black.png";
+import "./Navbar.css";
+
+const menuItems = ["service", "features", "pricing", "about us"];
 
 const Navbar = function () {
-  console.log(NavList);
   return (
     <nav className="navbar">
-      <NavList />
+      <div className="navbar__logo">
+        <img
+          src={logoBlack}
+          alt="Atom Bank logo"
+          className="navbar__logo-img"
+        />
+      </div>
+      <ul className="navbar__items">
+        {menuItems.map((item) => (
+          <NavItem menuItem={item} />
+        ))}
+      </ul>
+      <div className="navbar__btns">
+        <span className="navbar__btn">
+          <a href="#">Log In</a>
+        </span>
+        <span className="navbar__btn">
+          <a href="#">Register</a>
+        </span>
+      </div>
     </nav>
   );
+};
+
+const NavItem = function ({ menuItem }) {
+  return <li className="navbar__item">{menuItem}</li>;
 };
 export default Navbar;
