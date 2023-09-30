ad71ffeaad7498d9c3550ee697f022bf6c96878d
Kyeremeh Prince <kyeremehprince68@gmail.com>
2023-09-30 18:53:09 +0000

    Add css to Navbar component and modify code of Navbar component

--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -1,36 +1,38 @@
-import logoBlack from "./../assets/images/logo-black.png";
-import "./Navbar.css";
+import logoBlack from './../assets/images/logo-black.png';
+import './Navbar.css';
 
-const menuItems = ["service", "features", "pricing", "about us"];
+const menuItems = ['service', 'features', 'pricing', 'about us'];
 
 const Navbar = function () {
-  return (
-    <nav className="navbar">
-      <div className="navbar__logo">
-        <img
-          src={logoBlack}
-          alt="Atom Bank logo"
-          className="navbar__logo-img"
-        />
-      </div>
-      <ul className="navbar__items">
-        {menuItems.map((item) => (
-          <NavItem menuItem={item} />
-        ))}
-      </ul>
-      <div className="navbar__btns">
-        <span className="navbar__btn">
-          <a href="#">Log In</a>
-        </span>
-        <span className="navbar__btn">
-          <a href="#">Register</a>
-        </span>
-      </div>
-    </nav>
-  );
+	return (
+		<header className='header'>
+			<nav className='navbar'>
+				<div className='navbar__logo'>
+					<img
+						src={logoBlack}
+						alt='AtomBank logo'
+						className='navbar__logo-img'
+					/>
+				</div>
+				<ul className='navbar__items'>
+					{menuItems.map(item => (
+						<NavItem menuItem={item} />
+					))}
+				</ul>
+				<div className='navbar__btn-container'>
+					<button className='navbar__btn' type='button'>
+						Log In
+					</button>
+					<button className='navbar__btn' type='button'>
+						Register
+					</button>
+				</div>
+			</nav>
+		</header>
+	);
 };
 
 const NavItem = function ({ menuItem }) {
-  return <li className="navbar__item">{menuItem}</li>;
+	return <li className='navbar__item'>{menuItem}</li>;
 };
 export default Navbar;
