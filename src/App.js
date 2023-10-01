d73fe4b331e0855036bc33f4ec3fc26edb027a78
Kyeremeh Prince <kyeremehprince68@gmail.com>
2023-10-01 21:09:34 +0000

    Add Navbar, Hero, and Security components

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Navbar from './components/Navbar/Navbar';
+import Hero from './components/Hero/Hero';
+import Security from './components/Security/Security';
+
+function App() {
+	return (
+		<>
+			<Navbar />
+			<Hero />
+			<Security />
+		</>
+	);
+}
+export default App;
