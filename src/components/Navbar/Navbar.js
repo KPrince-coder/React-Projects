ae66ad65e2c84a2949722979c5f78328592cee19
Kyeremeh Prince <kyeremehprince68@gmail.com>
2023-10-01 01:23:58 +0000

    Add Navbar component and styles

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,41 @@
+import logoBlack from './../../assets/images/logo-black.png';
+import Button from '../Button/Button';
+import './Navbar.css';
+
+const menuItems = ['service', 'features', 'pricing', 'about us'];
+
+const Navbar = function () {
+	return (
+		<header className='header' id='header'>
+			<nav className='navbar'>
+				<div className='navbar__logo'>
+					<a href='#header'>
+						<img
+							src={logoBlack}
+							alt='AtomBank logo'
+							className='navbar__logo-img'
+						/>
+					</a>
+				</div>
+				<ul className='navbar__items'>
+					{menuItems.map((item, index) => (
+						<NavItem menuItem={item} key={index} />
+					))}
+				</ul>
+				<div className='navbar__btn-container'>
+					<Button type={'secondary'} text={'Log In'} />
+					<Button type={'primary'} text={'Register'} />
+				</div>
+			</nav>
+		</header>
+	);
+};
+
+const NavItem = function ({ menuItem }) {
+	return (
+		<li className='navbar__item'>
+			<a href='#header'>{menuItem}</a>
+		</li>
+	);
+};
+export default Navbar;
